1175d5aa33a2eda9026e933b7adefd80f1d8841a
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2013-06-30 16:14:07 -0300

    refactoring - ResponseHandler#respond_with_failure method

--- lib/braspag/response_handler.rb
+++ lib/braspag/response_handler.rb
@@ -6,42 +6,51 @@ module Braspag
   class ResponseHandler
     def authorize_transaction(response)
       data = response.body[:authorize_transaction_response][:authorize_transaction_result]
+
       if data[:success]
         payment_data_response = data[:payment_data_collection][:payment_data_response]
         status =  payment_data_response[:status]
+
         if AUTHORIZE_TRANSACTION_SUCCESS_STATUS.include? status
           respond_with_success(payment_data_response.merge(data[:order_data]))
         else
-          respond_with_failure(payment_data_response[:return_code], payment_data_response[:return_message])
+          respond_with_failure(data)
         end
+
       else
-        respond_with_failure_transaction(data)
+        respond_with_failure(data)
       end
     end
 
     def capture_transaction(response)
       data = response.body[:capture_credit_card_transaction_response][:capture_credit_card_transaction_result]
+
       if data[:success]
         data_collection = data[:transaction_data_collection][:transaction_data_response]
+
         if data_collection[:status] == CAPTURE_TRANSACTION_SUCCESS_STATUS
           respond_with_success(data_collection)
         else
-          respond_with_failure(data_collection[:return_code], data_collection[:return_message])
+          respond_with_failure(data)
         end
+
       else
-        respond_with_failure_transaction(data)
+        respond_with_failure(data)
       end
     end
 
     def void_transaction(response)
       data = response.body[:void_credit_card_transaction_response][:void_credit_card_transaction_result]
+
       if data[:success]
         data_collection = data[:transaction_data_collection][:transaction_data_response]
+
         if data_collection[:status] == VOID_TRANSACTION_SUCCESS_STATUS
           respond_with_success(data_collection)
         else
-          respond_with_failure(data_collection[:return_code], data_collection[:return_message])
+          respond_with_failure(data_collection)
         end
+
       else
         respond_with_failure_transaction(data)
       end
@@ -65,7 +74,7 @@ module Braspag
 
     def respond_with_failure_transaction(data)
       error_report = data[:error_report_data_collection][:error_report_data_response]
-      respond_with_failure(error_report[:error_code], error_report[:error_message])
+      respond_with_failure({:return_code => error_report[:error_code], :return_message => error_report[:error_message]})
     end
 
     def credit_card_response_for(data)
@@ -81,8 +90,13 @@ module Braspag
       OpenStruct.new(:success? => true, :data => data)
     end
 
-    def respond_with_failure(error_code, error_message)
-      OpenStruct.new(:success? => false, :error_code => error_code, :error_message => error_message)
+    def respond_with_failure(data)
+      data_response = data[:error_report_data_collection][:error_report_data_response]
+
+      OpenStruct.new(:success? => false,
+                     :error_code => data_response[:error_code],
+                     :error_message => data_response[:error_message],
+                     :error_data => data)
     end
   end
 end
--- spec/response_handler_spec.rb
+++ spec/response_handler_spec.rb
@@ -9,7 +9,7 @@ describe Braspag::ResponseHandler do
       response.data.should == failure_response
     end
   end
-  
+
   describe "save_credit_card" do
     describe "on success" do
       let(:body) do
@@ -45,10 +45,10 @@ describe Braspag::ResponseHandler do
          {:save_credit_card_result=>
           {:success=>false,
            :error_report_collection => 
-             {:error_report => 
-               {:error_code => return_code,
-               :error_message => return_message}}},
-           :@xmlns=>"http://www.cartaoprotegido.com.br/WebService/"}}
+          {:error_report => 
+           {:error_code => return_code,
+            :error_message => return_message}}},
+            :@xmlns=>"http://www.cartaoprotegido.com.br/WebService/"}}
       end
 
       let(:failure_response) do
@@ -60,7 +60,7 @@ describe Braspag::ResponseHandler do
         response.success?.should_not be_true
       end
 
-      it "returns the error_code and error_message" do
+      it "returns the error_code, error_message and error_data" do
         response = Braspag::ResponseHandler.new.save_credit_card failure_response
         response.error_code.should eq return_code
         response.error_message.should eq return_message
@@ -77,11 +77,11 @@ describe Braspag::ResponseHandler do
          {:get_credit_card_result=>
           {:success=>false,
            :error_report_collection => 
-             {:error_report => 
-               {:error_code => return_code,
-               :error_message => return_message}},
-           :masked_card_number=>"1"},
-           :@xmlns=>"http://www.cartaoprotegido.com.br/WebService/"}}
+          {:error_report => 
+           {:error_code => return_code,
+            :error_message => return_message}},
+            :masked_card_number=>"1"},
+            :@xmlns=>"http://www.cartaoprotegido.com.br/WebService/"}}
       end
 
       let(:failure_response) do
@@ -149,9 +149,9 @@ describe Braspag::ResponseHandler do
          {:capture_credit_card_transaction_result=>
           {:success => false,
            :error_report_data_collection => 
-             {:error_report_data_response => 
-               {:error_code => return_code,
-               :error_message => return_message}},
+          {:error_report_data_response => 
+           {:error_code => return_code,
+            :error_message => return_message}},
             :@xmlns=>"https://www.pagador.com.br/webservice/pagador"}}}
       end
 
@@ -170,6 +170,7 @@ describe Braspag::ResponseHandler do
         response.error_message.should eq return_message
       end
     end
+
     describe "on success" do
       let(:return_message) { "Operation Successful" }
       let(:return_code) { "6" }
@@ -191,7 +192,7 @@ describe Braspag::ResponseHandler do
            :error_report_data_collection=>nil,
            :transaction_data_collection=>
           {:transaction_data_response=> transaction_data_response }},
-            :@xmlns=>"https://www.pagador.com.br/webservice/pagador"}}
+          :@xmlns=>"https://www.pagador.com.br/webservice/pagador"}}
       end
 
       let(:success_response) do
@@ -199,6 +200,8 @@ describe Braspag::ResponseHandler do
       end
 
       describe "when the transaction status is 0" do
+
+
         it "returns success true" do
           response = Braspag::ResponseHandler.new.capture_transaction success_response
           response.success?.should be_true
@@ -211,6 +214,17 @@ describe Braspag::ResponseHandler do
       end
 
       describe "when the transaction status is not 0" do
+        let(:body) do
+          {:capture_credit_card_transaction_response=>
+           {:capture_credit_card_transaction_result=>
+            {:success => false,
+             :error_report_data_collection => 
+            {:error_report_data_response => 
+             {:error_code => return_code,
+              :error_message => return_message}},
+              :@xmlns=>"https://www.pagador.com.br/webservice/pagador"}}}
+        end
+
         before :each do
           transaction_data_response[:status] = 1
         end
@@ -227,107 +241,118 @@ describe Braspag::ResponseHandler do
         end
       end
     end
-  end
-  describe ".authorize_transaction" do
-    describe "on failure" do
-      let(:error_message){ "OrderId is a mandatory parameter" }
-      let(:error_code){ "405" }
-      let(:body) do
-        {:authorize_transaction_response=>
-         {:authorize_transaction_result=>
-          {:correlation_id=>"5735a1af-d26c-4716-bc8c-11da2990b8f5",
-           :success=>false,
-           :error_report_data_collection=>
-          {:error_report_data_response=>
-           {:error_code=> error_code,
-            :error_message=> error_message}}},
-            :@xmlns=>"https://www.pagador.com.br/webservice/pagador"}}
-      end
 
-      let(:failure_response) do
-        OpenStruct.new(:body => body)
-      end
-
-      it "returns success? false" do
-        response = Braspag::ResponseHandler.new.authorize_transaction failure_response
-        response.success?.should_not be_true
-      end
-
-      it "returns the error_code and error_message" do
-        response = Braspag::ResponseHandler.new.authorize_transaction failure_response
-        response.error_message.should == error_message
-        response.error_code.should == error_code
-      end
-    end
-
-    describe "on success" do
-      let(:payment_data_response) do
-        {:braspag_transaction_id=>"d1e74043-1ae1-4de6-ba4e-a34aaaf5cd8a",
-         :payment_method=>"997",
-         :amount=>"180",
-         :acquirer_transaction_id=>"0403102700500",
-         :authorization_code=>"938899",
-         :return_code=>"4",
-         :return_message=>"Operation Successful",
-         :status=>"1",
-         :credit_card_token=>"d94ad4a1-4c21-41e2-a63f-438e472cbbcf",
-         :"@xsi:type"=>"CreditCardDataResponse"}
-      end
-
-      let(:authorize_transaction_result) do
-        {:correlation_id=>"d014c828-2889-44c3-89e6-39004fc34c90",
-         :success=>true,
-         :error_report_data_collection=>nil,
-         :order_data=>
-        {:order_id=>"order98787174",
-         :braspag_order_id=>"d0e184a9-ecdb-417e-8a28-7d3b29a4d112"},
-         :payment_data_collection=>
-        {:payment_data_response=> payment_data_response}}
-      end
-
-      let(:body) do
-        {:authorize_transaction_response=>
-         {:authorize_transaction_result=> authorize_transaction_result,
-          :@xmlns=>"https://www.pagador.com.br/webservice/pagador"}}
-      end
-
-      let(:success_response) do
-        OpenStruct.new(:body => body)
-      end
-
-      let(:data) { authorize_transaction_result }
-      let(:return_message) { "invalid order id" }
-      let(:return_code) { "450" }
-
-      describe "when the transaction status is 1 or 2" do
-        it "returns a struct with the transaction data" do
-          expected_data = data[:payment_data_collection][:payment_data_response].merge(data[:order_data])
-          response = Braspag::ResponseHandler.new.authorize_transaction success_response
-          response.data.should == expected_data
+    describe ".authorize_transaction" do
+      describe "on failure" do
+        let(:error_message){ "OrderId is a mandatory parameter" }
+        let(:error_code){ "405" }
+        let(:body) do
+          {:authorize_transaction_response=>
+           {:authorize_transaction_result=>
+            {:correlation_id=>"5735a1af-d26c-4716-bc8c-11da2990b8f5",
+             :success=>false,
+             :error_report_data_collection=>
+            {:error_report_data_response=>
+             {:error_code=> error_code,
+              :error_message=> error_message}}},
+              :@xmlns=>"https://www.pagador.com.br/webservice/pagador"}}
         end
 
-        it "returns success true" do
-          response = Braspag::ResponseHandler.new.authorize_transaction success_response
-          response.success?.should be_true
-        end
-      end
-
-      describe "when the transaction status is not 1 or 2" do
-        before :each do
-          payment_data_response[:status] = 3
-          payment_data_response[:return_message] = return_message
-          payment_data_response[:return_code] = return_code
+        let(:failure_response) do
+          OpenStruct.new(:body => body)
         end
 
-        it "returns success false" do
-          response = Braspag::ResponseHandler.new.authorize_transaction success_response
+        it "returns success? false" do
+          response = Braspag::ResponseHandler.new.authorize_transaction failure_response
           response.success?.should_not be_true
         end
 
         it "returns the error_code and error_message" do
-          response = Braspag::ResponseHandler.new.authorize_transaction success_response
-          response.error_code.should == return_code
-          response.error_message.should == return_message
+          response = Braspag::ResponseHandler.new.authorize_transaction failure_response
+          response.error_message.should == error_message
+          response.error_code.should == error_code
+        end
+      end
+
+      describe "on success" do
+        let(:payment_data_response) do
+          {:braspag_transaction_id=>"d1e74043-1ae1-4de6-ba4e-a34aaaf5cd8a",
+           :payment_method=>"997",
+           :amount=>"180",
+           :acquirer_transaction_id=>"0403102700500",
+           :authorization_code=>"938899",
+           :return_code=>"4",
+           :return_message=>"Operation Successful",
+           :status=>"1",
+           :credit_card_token=>"d94ad4a1-4c21-41e2-a63f-438e472cbbcf",
+           :"@xsi:type"=>"CreditCardDataResponse"}
+        end
+
+        let(:authorize_transaction_result) do
+          {:correlation_id=>"d014c828-2889-44c3-89e6-39004fc34c90",
+           :success=>true,
+           :error_report_data_collection=>nil,
+           :order_data=>
+          {:order_id=>"order98787174",
+           :braspag_order_id=>"d0e184a9-ecdb-417e-8a28-7d3b29a4d112"},
+           :payment_data_collection=>
+          {:payment_data_response=> payment_data_response}}
+        end
+
+        let(:body) do
+          {:authorize_transaction_response=>
+           {:authorize_transaction_result=> authorize_transaction_result,
+            :@xmlns=>"https://www.pagador.com.br/webservice/pagador"}}
+        end
+
+        let(:success_response) do
+          OpenStruct.new(:body => body)
+        end
+
+        let(:data) { authorize_transaction_result }
+        let(:return_message) { "invalid order id" }
+        let(:return_code) { "450" }
+
+        describe "when the transaction status is 1 or 2" do
+          it "returns a struct with the transaction data" do
+            expected_data = data[:payment_data_collection][:payment_data_response].merge(data[:order_data])
+            response = Braspag::ResponseHandler.new.authorize_transaction success_response
+            response.data.should == expected_data
+          end
+
+          it "returns success true" do
+            response = Braspag::ResponseHandler.new.authorize_transaction success_response
+            response.success?.should be_true
+          end
+        end
+
+        describe "when the transaction status is not 1 or 2" do
+          let(:body) do
+            {:authorize_transaction_response=>
+             {:authorize_transaction_result=>
+              {:correlation_id=>"5735a1af-d26c-4716-bc8c-11da2990b8f5",
+               :success=>false,
+               :error_report_data_collection=>
+              {:error_report_data_response=>
+               {:error_code=> return_code,
+                :error_message=> return_message}}},
+                :@xmlns=>"https://www.pagador.com.br/webservice/pagador"}}
+          end
+
+          before :each do
+            payment_data_response[:status] = 3
+          end
+
+          it "returns success false" do
+            response = Braspag::ResponseHandler.new.authorize_transaction success_response
+            response.success?.should_not be_true
+          end
+
+          it "returns the error_code and error_message" do
+            response = Braspag::ResponseHandler.new.authorize_transaction success_response
+            response.error_code.should == return_code
+            response.error_message.should == return_message
+          end
         end
       end
     end
